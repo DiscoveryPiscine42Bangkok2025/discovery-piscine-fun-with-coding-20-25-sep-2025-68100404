0c1249c55f5bd5be42571e48fc8042ad00022e88
68100404 <68100404@kmitl.ac.th>
2025-09-22 22:21:32 +0700

    Create build.sh

---
+++ cell00/build.sh
@@ -0,0 +1,10 @@
+ex05/ build.sh  if [ $# -eq 0]; then
+    echo "No arguments supplied"
+else
+    for i in "$@"
+    do
+        if [ -n "$@" ]; then
+            mkdir ex$i
+        fi
+    done
+fi
