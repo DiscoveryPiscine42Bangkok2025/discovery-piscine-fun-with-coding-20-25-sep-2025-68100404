63367ba1bb96f77494db2e32b2433c5d19c7dc33
68100404 <68100404@kmitl.ac.th>
2025-09-22 21:49:24 +0700

    Create argv.sh

---
+++ cell00/argv.sh
@@ -0,0 +1,10 @@
+ex04/argv.sh        if [ $# -eq 0 ]; then
+    echo "NO arguments supplied"
+else
+    for i in "$1" "$2" "$3"
+    do
+            if [ -n "$i" ]; then
+                echo "$i"
+            fi
+    done
+fi
